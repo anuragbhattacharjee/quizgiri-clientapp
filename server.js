42dc4a5f183e4ba3a516a561d35f6f90c7e7044a
Anurag Bhattacharjee <anurag07061@gmail.com>
2018-07-24 18:17:25 +0600

    express server.js edit file path

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/quizgiri-clientapp'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/quizgiri-clientapp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ndex.html'));
 });
+
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
